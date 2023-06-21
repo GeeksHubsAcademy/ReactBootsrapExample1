3e66b713e92187f385071898a930cac2cddc1c3f
Dave86dev <davidochando1986@gmail.com>
2023-06-21 12:27:59 +0200

    React bootstrap ejemplo básico de implementación

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { Home } from './pages/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import "./Home.css";
+
+import { NavBar } from '../../common/NavBar/NavBar';
+ 
+export const Home = () => {
+     return (
+         <div className='homeDesign'>
+            <NavBar />
+         </div>
+     )
+}
